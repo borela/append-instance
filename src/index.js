95a8b202e0371c59bf80cc15ac48a32d195777eb
Alexandre Borela <alexandre.borela@gmail.com>
2017-09-12 10:37:45 -0300

    Implement decorator

---
+++ src/index.js
@@ -0,0 +1,53 @@
+// Licensed under the Apache License, Version 2.0 (the “License”); you may not
+// use this file except in compliance with the License. You may obtain a copy of
+// the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an “AS IS” BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations under
+// the License.
+// @flow
+
+import { Component } from 'react'
+import partialRight from 'lodash.partialright'
+
+const HANDLER_IDENTIFIER = /^on[A-Z]\w*/
+
+export function appendInstance(targetComponent:Class<Component>) {
+  let prototype = targetComponent.prototype
+
+  prototype.raiseEvent = function(event, ...args) {
+    let handler = this.props[event]
+    if (handler)
+      handler(...args, this)
+  }
+
+  prototype.raiseEvents = function(events, ...args) {
+    for (let event of events) {
+      let handler = this.props[event]
+      if (handler)
+        handler(...args, this)
+    }
+  }
+
+  let oldGetThemeableData = prototype.getThemeableData
+  prototype.getThemeableData = function() {
+    let result = oldGetThemeableData.call(this)
+    let props = result.props
+    for (let propName in result.props) {
+      if (!HANDLER_IDENTIFIER.test(propName))
+        continue
+      let oldHandler = props[propName]
+      props[propName] = partialRight(oldHandler, this)
+    }
+
+    return result
+  }
+
+  return targetComponent
+}
+
+export default appendInstance
